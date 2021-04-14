7cce3ff2676be11a9a80e76b57bf201d43616cfe
Maia Vasaturo-Kolodner <maia_vasaturo-kolodner@brown.edu>
2021-04-14 13:33:44 -0400

    Maia: implementing AI game lines 16-23: creating static variables and replacing them throughout the code lines 74- 105: creating score incentives for getting 3 or 4 pieces in a row horizontally; determining whether or not a drop location is valid and open; defining what the best move for the AI should be using the score function as defined above; creating new board so that original will remain untouched; keeping track of score to inform future best move choices line 143: randomize who starts the game line 180-182: reboot game board after AI turn line 185: changing from active second player to AI line 190: wait 0.5 seconds to drop AI piece line 197: changed Player B to Computer
    general debugging of problems as they occurred (ie colors were flipped, import issues, variable definitions and their inconsistencies, variable ordering within functions, and more)
    
    Maia total hours worked: 17.5 (7 hours of research, 8 hours of coding, 2.5 hours of debugging)
    Neil observed for 8 hours of coding, 2 hours of debugging, and helped me with research for 2 hours
    Mac helped me with research for 5 hours.
    
    Note: my research and debugging hours for my last commit were not recorded--I only reported my hours worth of coding.
    Hours researched for 4/6 commit: 4 hours
    Debugging for 4/6 commit: 2 hours
    Therefore, total hours were ~12. No one observed/helped my debugging or research hours

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 import pygame
 import math
+import random
 
 Yellow = (255,255,0) # sets color yellow for use in visual board
 Black = (0,0,0) # sets color black for use in visual board
@@ -12,6 +13,14 @@ Green = (0,255,0) # sets color green for use in visual board
 row_count = 6
 col_count = 7
 
+PlayerA = 1
+AI = 0
+
+PlayerA_piece = 1
+AI_piece = 2
+
+block_len = 4
+none = 0
 
 # Create matrix of zeros to initialize game board
 def c4_board(row_count, col_count):
@@ -62,6 +71,39 @@ def win(game_board, game_piece):
             if game_board[r][c] == game_piece and game_board[r-1][c+1] == game_piece and game_board[r-2][c+2] == game_piece and game_board[r-3][c+3] == game_piece: #if all the values are the same on the leftward diagonal
                 return True
 
+def score_pos(game_board, game_piece):
+    score = 0
+    for r in range(row_count): # Horizontal score
+        row_array = [int(i) for i in list(game_board[r,:])] # indexing specific row and all its columnns
+        for c in range(col_count-3):
+            block = row_array[c:block_len]
+            if block.count(game_piece) == 4: # if 4 in a row increase score by 100
+                score += 100
+            elif block.count(game_piece) == 3 and block.count(none) == 1: # if 3 in a row, increase score by 10
+                score += 10
+    return score
+
+def get_val_loc(game_board): # determine whether or not the column choice is valid and open
+    val_loc = []
+    for col_selection in range(col_count):
+        if droploc_is_val(game_board, col_selection):
+            val_loc.append(col_selection)
+    return val_loc
+
+def best_move(game_board, game_piece): # determine what the next best move is
+    val_loc = get_val_loc(game_board)
+    best_score = 0
+    best_col_selection = random.choice(val_loc)
+    for col_selection in val_loc:
+        row = find_next_open_row(game_board, col_selection)
+        new_board = game_board.copy() #create new memory location
+        drop_game_piece(new_board, row, col_selection, game_piece) #pass new memory location into our drop function
+        score = score_pos(new_board, game_piece) #find score of new board
+        if score > best_score: #keep track of score
+            best_score = score
+            best_col_selection = col_selection
+    return best_col_selection
+
 
 def draw_board(game_board): # function to draw game board with set sizes based on for loops with c and r
     for c in range(col_count):
@@ -70,9 +112,9 @@ def draw_board(game_board): # function to draw game board with set sizes based o
             pygame.draw.circle(screen, Black, (int(c*square_size+square_size/2), int(r*square_size+square_size+square_size/2)), radius) # draws the circular portion of the game board
     for c in range(col_count):  # nested for loop that checks every spot on board to see if a piece is present
         for r in range(row_count):
-            if game_board[r][c] == 1:  # if player A has a piece in a certain spot, draw a red circle in that spot
+            if game_board[r][c] == PlayerA_piece:  # if player A has a piece in a certain spot, draw a red circle in that spot
                 pygame.draw.circle(screen, Red, (int(c * square_size + square_size / 2), height-int(r * square_size + square_size / 2)), radius)
-            elif game_board[r][c] == 2:  # if player B has a piece in a certain spot, draw a green circle in that spot
+            elif game_board[r][c] == AI_piece:  # if player B has a piece in a certain spot, draw a green circle in that spot
                 pygame.draw.circle(screen, Green, (int(c * square_size + square_size / 2), height-int(r * square_size + square_size / 2)), radius)
     pygame.display.update()  # once circles are drawn, update game board
 
@@ -81,8 +123,6 @@ game_board = c4_board(row_count, col_count)
 
 # initializing game variables
 game_is_over = False
-player_turn = 1
-turn = 0
 
 pygame.init()
 
@@ -100,6 +140,8 @@ pygame.display.update()  # updates the game board to new visual attributes
 
 Win_Font = pygame.font.SysFont("comicsansms", 55)
 
+player_turn = random.randint(AI, PlayerA) # randomize who goes first
+
 while not game_is_over:  # while the game is not over, continues loop
 
     for event in pygame.event.get():  # initializes the different events that will be looked for in game
@@ -108,17 +150,15 @@ while not game_is_over:  # while the game is not over, continues loop
         if event.type == pygame.MOUSEMOTION:  # creates function that has the piece follow the mouse along the top rect
             pygame.draw.rect(screen, Black, (0,0,width,square_size))  # draws black rectangle so that only one copy of piece appears
             posx = event.pos[0]
-            if player_turn == 1:  # if it is player A's turn, red piece
+            if player_turn == PlayerA:  # if it is player A's turn, red piece
                 pygame.draw.circle(screen, Red, (posx, int(square_size/2)), radius)
-            else:  # if it is player B's turn, green piece
-                pygame.draw.circle(screen, Green, (posx, int(square_size / 2)), radius)
         pygame.display.update()
 
         if event.type == pygame.MOUSEBUTTONDOWN: ## initializes the type of event (clicking mouse) to make changes in game
             pygame.draw.rect(screen, Black, (0, 0, width, square_size))
             # print(event.pos)
             # Player A user input
-            if player_turn % 2 == 1:  # If value of player turn is odd
+            if player_turn == PlayerA:  # If value of player turn is odd
                 posx = event.pos[0]    # initializes the click of player to x position of click
                 col_selection = int(math.floor(posx/square_size)) # sets clicks to align with the set columns
 
@@ -126,34 +166,47 @@ while not game_is_over:  # while the game is not over, continues loop
                     row = find_next_open_row(game_board,
                                              col_selection)  # Making row equal to the output of find_next_open_row
                     drop_game_piece(game_board, row, col_selection,
-                                    1)  # Drop game piece with value 1 at specified location
+                                    PlayerA_piece)  # Drop game piece with value 1 at specified location
 
-                    if win(game_board, 1):  # if player A wins, print win statement at top of game board in red color
+                    if win(game_board, PlayerA_piece):  # if player A wins, print win statement at top of game board in red color
                         win_label = Win_Font.render("PlAYER A IS THE CHAMP",1,Red)  # create win label in red
                         screen.blit(win_label, (35,10))  # print win label at top of board
                         game_is_over = True  # end game
 
-            # Player B user input
-            else:  # If value of player turn is odd
-                posx = event.pos[0]  # initializes the click of player to x position of click
-                col_selection = int(math.floor(posx / square_size))  # sets clicks to align with the set columns
+                    # once AI turn is completed, update turn
+                    player_turn += 1
+                    player_turn = player_turn % 2
 
-                if droploc_is_val(game_board, col_selection):  # If the drop location is valid (row isn't full
-                    row = find_next_open_row(game_board,
-                                             col_selection)  # If the drop location is valid (row isn't full)
-                    drop_game_piece(game_board, row, col_selection,
-                                    2)  # Drop game piece with value 2 at specified location
+                    # flip and then draw the game board after each turn to update
+                    flip_board(game_board)
+                    draw_board(game_board)
 
-                    if win(game_board, 2):  # if player B wins print win statement at top of game board
-                        win_label = Win_Font.render("PlAYER B IS THE CHAMP", 1, Green)  # create win label in green
-                        screen.blit(win_label, (35, 10))  # print win label at top of board
-                        game_is_over = True  # end game once player wins
-            # flip and then draw the game board after each turn to update
-            flip_board(game_board)
-            draw_board(game_board)
-            # once a players turn is completed, update turn
-            player_turn += 1
-            player_turn = player_turn % 2
+            # AI user input
+        if player_turn == AI and not game_is_over: # initialize AI
+            # col_selection = random.randint(0, col_count - 1) # randomly select row
+            col_selection = best_move(game_board, AI_piece)
 
-            if game_is_over:  # when game ends, wait 5 seconds so that players can read the win statement
-                pygame.time.wait(5000)
+            if droploc_is_val(game_board, col_selection):  # If the drop location is valid (row isn't full)
+                pygame.time.wait(500) # wait 0.5seconds to place AI chip
+                row = find_next_open_row(game_board,
+                                         col_selection)  # If the drop location is valid (row isn't full)
+                drop_game_piece(game_board, row, col_selection,
+                                AI_piece)  # Drop game piece with value 2 at specified location
+
+                if win(game_board, AI_piece):  # if player B wins print win statement at top of game board
+                    win_label = Win_Font.render("COMPUTER IS THE CHAMP", 1, Green)  # create win label in green
+                    screen.blit(win_label, (35, 10))  # print win label at top of board
+                    game_is_over = True  # end game once player wins
+
+                # flip and then draw the game board after each turn to update
+                flip_board(game_board)
+                draw_board(game_board)
+
+                # once a players turn is completed, update turn
+                player_turn += 1
+                player_turn = player_turn % 2
+
+
+
+    if game_is_over:  # when game ends, wait 5 seconds so that players can read the win statement
+        pygame.time.wait(5000)
